ce9b1e6ebb5d5beb4b3f143dea226526e8d56f31
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-13 15:42:49 +0530

    practice server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import express from "express";
-const app = express();
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import { UserModel } from "./database";
 
-app.post('/v1/signup' ,(req ,res)=>{
-    res.send("hi");
-})
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  await UserModel.create({
+    email: email,
+    password: password
+  });
+  res.json({
+    msg: "signed up sucessfully",
+  });
+});
+
+async function main() {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URL}`);
+    console.log("database connected");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`app listening to port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("database connection error", error);
+  }
+}
+
+main();
